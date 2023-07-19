d6bfeb39e557f78f78306e2347aac59a556277c6
ryo ono <ryo.ono@gmail.com>
2023-07-19 14:29:36 +0900

    Fix_全体をリファクタリング

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,5 @@
 class MapsController < ApplicationController
-  before_action :set_map, only: %i[ show edit update destroy ]
+  before_action :set_map, only: %i[ show edit  destroy ]
 
   def index
     @maps = Map.all
@@ -13,52 +13,30 @@ class MapsController < ApplicationController
     @map = Map.new
   end
 
-  def edit
-  end
-
   def create
     @map = Map.new(map_params)
 
     respond_to do |format|
       if @map.save
         format.html { redirect_to maps_url, notice: "Map was successfully created." }
-        format.json { render :show, status: :created, location: @map }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @map.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  def update
-    respond_to do |format|
-      if @map.update(map_params)
-        format.html { redirect_to map_url(@map), notice: "Map was successfully updated." }
-        format.json { render :show, status: :ok, location: @map }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @map.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /maps/1 or /maps/1.json
   def destroy
     @map.destroy
-
     respond_to do |format|
       format.html { redirect_to maps_url, notice: "Map was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_map
       @map = Map.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def map_params
       params.require(:map).permit(:name, :latitude, :longitude)
     end
