f20e11ac38001944bd3882111753210c4694bcf7
ryo ono <ryo.ono@gmail.com>
2023-07-20 18:28:58 +0900

    Add_familyの新規作成欄

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,4 +1,4 @@
 class Family < ApplicationRecord
-  enum sex: { 男: 0, その他: 1, 女:2}
-  enum diet: { 多め: 0, 普通: 1, 少なめ:2}
+  enum sex: { 女性: 0, 男性: 1, その他:2, 回答しない: 3}
+  enum diet: { 少なめ: 0, 普通: 1, 多め:2}
 end
