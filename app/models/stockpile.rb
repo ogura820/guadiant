48f017bcb4cbb9e9ff57235f5b7b67619ca839ca
ryo ono <ryo.ono@gmail.com>
2023-08-01 18:20:07 +0900

    Add_wheneverの動作確認完了

--- app/models/stockpile.rb
+++ app/models/stockpile.rb
@@ -1,3 +1,14 @@
 class Stockpile < ApplicationRecord
   belongs_to :user
+
+  def self.notice_on_check
+    Stockpile.all.each do |stockpile|
+      if stockpile.notice_on && stockpile.notice_on.to_date < Time.now.to_date
+        stockpile.status = true
+        stockpile.save
+      end
+    end
+    Rails.logger.info "Stockpile.notice_on_check executed at #{Time.now}"
+  end
+
 end
