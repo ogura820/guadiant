f411a977836ce5d0d4c4faf84859d8d2013067d6
ryo ono <ryo.ono@gmail.com>
2023-08-02 11:50:40 +0900

    Add_通知日にメールが来る機能

--- app/models/stockpile.rb
+++ app/models/stockpile.rb
@@ -3,8 +3,9 @@ class Stockpile < ApplicationRecord
 
   def self.notice_on_check
     Stockpile.all.each do |stockpile|
-      if stockpile.notice_on && stockpile.notice_on.to_date < Time.now.to_date
+      if stockpile.notice_on && stockpile.notice_on.to_date < Time.now.to_date && stockpile.status = false
         stockpile.status = true
+        StockpileMailer.notice_mail(stockpile).deliver
         stockpile.save
       end
     end
