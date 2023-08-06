d805f1c7dcc5a88ed84bdbd901e18b58fabe6121
ryo ono <ryo.ono@gmail.com>
2023-08-06 16:53:11 +0900

    Fix_stockpilesの日にちが空白の時の処理をhelperに移動

--- app/helpers/stockpiles_helper.rb
+++ app/helpers/stockpiles_helper.rb
@@ -1,2 +1,11 @@
 module StockpilesHelper
+  
+  def stockpile_strftime_text(stockpile)
+    if stockpile == nil 
+      "指定なし" 
+    else
+      stockpile.strftime("%Y年%m月%d日") 
+    end
+  end
+
 end
