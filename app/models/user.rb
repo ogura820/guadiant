bc00446ec3bf1c9bc57b829a8b643d04568aadd9
ryo ono <ryo.ono@gmail.com>
2023-08-08 17:12:53 +0900

    Fix_インデント

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,11 +14,11 @@ class User < ApplicationRecord
     end
   end
 
-    def self.admin_guest
-      find_or_create_by!(email: 'admin@example.com', admin: true) do |user|
-        user.password = SecureRandom.urlsafe_base64
-      end
+  def self.admin_guest
+    find_or_create_by!(email: 'admin@example.com', admin: true) do |user|
+      user.password = SecureRandom.urlsafe_base64
     end
+  end
 
   def calculate_stocks
     {
