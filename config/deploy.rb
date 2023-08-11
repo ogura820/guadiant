057a1f5727fe60cdad03d63a9199f06a74c85a2a
ryo ono <ryo.ono@gmail.com>
2023-08-11 15:14:37 +0900

    Fix_AWS設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.16.0'
 
 # デプロイするアプリケーション名
-set :application, 'Guadiant'
+set :application, 'Guardiant'
 
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
@@ -12,7 +12,7 @@ set :repo_url, 'https://github.com/ogura820/guadiant'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/guadiant'
+set :deploy_to, '/var/www/guardiant'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
