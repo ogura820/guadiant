2fdf92cb632f1dfb73ff09059a8faee5c145e49e
ryo ono <ryo.ono@gmail.com>
2023-08-11 15:10:13 +0900

    Fix_deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.16.0'
 
 # デプロイするアプリケーション名
-set :application, 'Guardiant'
+set :application, 'Guadiant'
 
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
@@ -12,7 +12,7 @@ set :repo_url, 'https://github.com/ogura820/guadiant'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/guardiant'
+set :deploy_to, '/var/www/guadiant'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
