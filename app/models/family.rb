7db82a638013a37d85dcd78edd32f1ce3b4b05ad
ryo ono <ryo.ono@gmail.com>
2023-08-13 09:41:45 +0900

    Fix_各ページのタイトルロゴを修正

--- app/models/family.rb
+++ app/models/family.rb
@@ -44,37 +44,37 @@ class Family < ApplicationRecord
     age_coefficient
   end
 
-  # def self.generate_story(current_user)
-  #   client = OpenAI::Client.new(access_token: ENV["CHATGPT_API_KEY"])
+  def self.generate_story(current_user)
+    client = OpenAI::Client.new(access_token: ENV["CHATGPT_API_KEY"])
 
-  #   family_info = current_user.families.map do |f|
-  #           {
-  #             name: f.name,
-  #             sex: f.sex,
-  #             age: f.age,
-  #             diet: f.diet,
-  #             pet: f.pet
-  #             }
-  #   end
-  #   stocks_info = current_user.calculate_stocks 
-  #     content = "あなたは経験豊富な防災関連の専門家です。可能な限りメンバーの年齢・性別・食生活・ペットか否かを考慮して質問に答えてください."
-  #     family_info.each do |info|
-  #       content += "メンバーは#{info[:name]}さんは#{info[:age]}歳の#{info[:sex]}で、食生活は#{info[:diet]}です。ペット（動物）か否かは#{info[:pet]}"
-  #     end
+    family_info = current_user.families.map do |f|
+            {
+              name: f.name,
+              sex: f.sex,
+              age: f.age,
+              diet: f.diet,
+              pet: f.pet
+              }
+    end
+    stocks_info = current_user.calculate_stocks 
+      content = "あなたは経験豊富な防災関連の専門家です。可能な限りメンバーの年齢・性別・食生活・ペットか否かを考慮して質問に答えてください."
+      family_info.each do |info|
+        content += "メンバーは#{info[:name]}さんは#{info[:age]}歳の#{info[:sex]}で、食生活は#{info[:diet]}です。ペット（動物）か否かは#{info[:pet]}"
+      end
     
-  #     content += "防災備蓄として以下の物資を保有しています："
-  #     stocks_info.each do |item, data|
-  #       content += "【#{item}】  一人分: #{data[:一人分]}。"
-  #     end
+      content += "防災備蓄として以下の物資を保有しています："
+      stocks_info.each do |item, data|
+        content += "【#{item}】  一人分: #{data[:一人分]}。"
+      end
 
-  #     content += "他に必要な物資の名前と簡単な理由を教えてください。100文字以内で保有している物資については回答に含めないでください。出力は、[(物資名)：(簡単な理由)]で3個書いてください。"
-  #     response = client.chat(
-  #                           parameters: {
-  #                                         model: 'gpt-3.5-turbo',
-  #                                         messages: [{ role: 'user',content: "#{content}" }],
-  #                                       }
-  #                           )
-  #     response.dig('choices', 0, 'message', 'content')
-  # end
+      content += "他に必要な物資の名前と簡単な理由を教えてください。100文字以内で保有している物資については回答に含めないでください。出力は、[(物資名)：(簡単な理由)]で3個書いてください。"
+      response = client.chat(
+                            parameters: {
+                                          model: 'gpt-3.5-turbo',
+                                          messages: [{ role: 'user',content: "#{content}" }],
+                                        }
+                            )
+      response.dig('choices', 0, 'message', 'content')
+  end
 
 end
