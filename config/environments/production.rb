d635d93866da4fd5c55e0da84dfbef24cc247dac
ryo ono <ryo.ono@gmail.com>
2023-08-14 14:43:48 +0900

    Fic_port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
     password: ENV['SENDGRID_PASSWORD'],
     domain: "guardiant.tokyo",
     address: "smtp.sendgrid.net",
-    port: 25,
+    port: 465,
     authentication: :plain,
     enable_starttls_auto: true
   }
