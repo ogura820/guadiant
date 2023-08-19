26eb37000e267722a004aa193e07bf5913473803
ryo ono <ryo.ono@gmail.com>
2023-08-20 06:25:13 +0900

    Test_whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,9 +5,14 @@ rails_env = ENV['RAILS_ENV'] || :development
 set :output, "#{Rails.root}/log/cron.log"
 set :environment, rails_env
 
-every 1.day, at: '6:20 am' do
+# every 1.day, at: '6:20 am' do
+#   runner "Stockpile.notice_on_check"
+# end
+
+every 1.minute do
   runner "Stockpile.notice_on_check"
 end
+
 
 every '0 0 1 1 *' do
   runner 'Family.increace_age'
